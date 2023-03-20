bb4608ac3a267b85d7e9b0b96311d49e27bc91b1
KlassArtem <1trabl123@gmail.com>
2023-03-20 11:07:57 +0200

    upd

--- src/helpers/api/index.js
+++ src/helpers/api/index.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 
 export async function fetchImages(value, page = 1) {
   const searchParams = new URLSearchParams({
-    key: `33064708-e869c07a3c852e671a2fdaacb`,
+    key: `33354076-0a876f5b723d518ba8fae8563`,
     image_type: 'photo',
     orientation: 'horizontal',
     page,
